1b438f4005ec708dd3db85294d7692b03982765b
Ronesha Egson <ro.egson@gmail.com>
2017-11-13 12:43:40 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*var menu = document.querySelector(".drowpdown-menu");
+var drop = document.querySelector(".dropdwn");
+drop.addEventListener("click" function(){
+ 
+       menu.toggle.style.display = "block";
+
+
+})*/
+
+var drop = document.querySelector(".dropdwn");
+
+drop.addEventListener("click", function(){
+	alert("clicky");
+})
